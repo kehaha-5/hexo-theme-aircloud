b01519a1d13612dce1e460569fab4144a39f840e
unknown <1011700940@qq.com>
2024-03-05 21:29:52 +0800

    添加了fancybox的支持

---
+++ source/js/fancybox/wrapImage.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    wrapImageWithFancyBox();
+});
+
+/**
+ * Wrap images with fancybox support.
+ * edit from https://tianma8023.github.io/post/hexo-material-intergrate-image-display-feature/
+ */
+function wrapImageWithFancyBox() {
+    $('img').each(function () {
+        let image = $(this);
+        //头像
+        if (image.parent("div") && image.parent("div").hasClass("avatar")) {
+            return;
+        }
+        let tmpImgHtml = image.prop("outerHTML");
+        let html = `<a data-fancybox="images" href=${image.attr("src")}>${tmpImgHtml}</a>`;
+        let contextImageParent = image.parent("p");
+        image.remove();
+        contextImageParent.append(html);
+    });
+
+    $().fancybox({
+        selector: '[data-fancybox="images"]',
+        thumbs: false,
+        hash: true,
+        loop: false,
+        fullScreen: false,
+        slideShow: false,
+        protect: true,
+    });
+}
